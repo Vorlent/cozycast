7e2104477f912781b12f50e79cbb06e6e475757e
Vorlent <vorlent@web.de>
2019-10-15 13:39:16 +0200

    Added restart functionality for the lua worker

--- cozycast-worker/worker.lua
+++ cozycast-worker/worker.lua
@@ -17,42 +17,49 @@ local keyboard_web_to_xdo = {
   ["ArrowDown"] = "Down",
   ["Backspace"] = "BackSpace",
 }
+function start_server()
+    local ws = websocket.new_from_uri("ws://localhost:8080/stream")
+    ws:connect()
+    ws:send(lunajson.encode{ action = "worker"})
+    while true do
+      local msg = ws:receive()
+      local data = lunajson.decode(msg)
+      if data.action == "mousemove" then
+        os.execute ("xdotool mousemove "..data.mouseX.." "..data.mouseY)
+      end
+      if data.action == "mouseup" then
+        os.execute ("xdotool mousemove "..data.mouseX.." "..data.mouseY)
+        os.execute ("xdotool mouseup "..(mouse_web_to_xdo[data.button]))
+      end
+      if data.action == "mousedown" then
+        os.execute ("xdotool mousemove "..data.mouseX.." "..data.mouseY)
+        os.execute ("xdotool mousedown "..(mouse_web_to_xdo[data.button]))
+      end
 
-local ws = websocket.new_from_uri("ws://localhost:8080/stream")
-ws:connect()
-ws:send(lunajson.encode{ action = "worker"})
-while true do
-  local msg = ws:receive()
-  local data = lunajson.decode(msg)
-  if data.action == "mousemove" then
-    os.execute ("xdotool mousemove "..data.mouseX.." "..data.mouseY)
-  end
-  if data.action == "mouseup" then
-    os.execute ("xdotool mousemove "..data.mouseX.." "..data.mouseY)
-    os.execute ("xdotool mouseup "..(mouse_web_to_xdo[data.button]))
-  end
-  if data.action == "mousedown" then
-    os.execute ("xdotool mousemove "..data.mouseX.." "..data.mouseY)
-    os.execute ("xdotool mousedown "..(mouse_web_to_xdo[data.button]))
-  end
+      if data.action == "keyup" then
+        data.key = keyboard_web_to_xdo[data.key] or data.key
+        os.execute ("xdotool keyup "..data.key)
+      end
+      if data.action == "keydown" then
+        data.key = keyboard_web_to_xdo[data.key] or data.key
+        os.execute ("xdotool keydown "..data.key)
+      end
+      if data.action == "scroll" then
+        print(data)
+        if data.direction == "up" then
+          os.execute ("xdotool click 4")
+        end
+        if data.direction == "down" then
+          os.execute ("xdotool click 5")
+        end
+      end
+    end
 
-  if data.action == "keyup" then
-    data.key = keyboard_web_to_xdo[data.key] or data.key
-    os.execute ("xdotool keyup "..data.key)
-  end
-  if data.action == "keydown" then
-    data.key = keyboard_web_to_xdo[data.key] or data.key
-    os.execute ("xdotool keydown "..data.key)
-  end
-  if data.action == "scroll" then
-    print(data)
-    if data.direction == "up" then
-      os.execute ("xdotool click 4")
-    end
-    if data.direction == "down" then
-      os.execute ("xdotool click 5")
-    end
-  end
+    ws:close()
 end
 
-ws:close()
+while true do
+    pcall(start_server)
+    print("Restarting lua worker in 5 seconds")
+    os.execute("sleep 5")
+end
