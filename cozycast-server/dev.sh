7fd5cc9a3ac4ec0b3e5b130031c9884c5c6ee08c
Vorlent <vorlent@web.de>
2019-11-12 00:22:37 +0100

    Use docker container for development with cache mount

--- cozycast-server/dev.sh
+++ cozycast-server/dev.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+#!/bin/bash
 source ../.env
-export MAVEN_OPTS="-Xmx100m -Xms50m"
-./mvnw compile exec:java
+mkdir -p cache
+sudo docker run \
+  --rm \
+  -it \
+  --env TURN_SECRET=$TURN_SECRET \
+  --env TURN_IP=$TURN_IP \
+  --env KURENTO_IP=$KURENTO_IP \
+  --network host \
+  -v $(realpath .):/root/cozycast/cozycast-server \
+  -v $(realpath cache/.m2):/root/.m2/repository \
+  --name cozycast-server cozycast-server \
+  mvn compile exec:java
+
+# Ports required:
+# TCP 8443 Webserver
+# TCP 8080 Webserver
