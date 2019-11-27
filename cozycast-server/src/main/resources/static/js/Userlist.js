5180cb2eb84d8753a09de17919c9e02a5890ed5e
Vorlent <vorlent@web.de>
2019-11-27 23:38:55 +0100

    Ensure profile and userlist use identical styling

--- cozycast-server/src/main/resources/static/js/Userlist.js
+++ cozycast-server/src/main/resources/static/js/Userlist.js
@@ -4,7 +4,7 @@ export function Userlist({ state }) {
     return html`<div id="userlist" class="userlist">
         ${state.userlist.map(user => html`
             <div class="user">
-                <img class="avatar" src="${user.url}"></img>
+                <div class="image avatar" style="background-image: url('${user.url}');"/>
                 <div class="centered">${user.username}</div>
                 <i class="icon-keyboard remote" style=${user.remote ? "" : "display: none;"}></i>
             </div>
