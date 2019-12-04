4327c3fce6fb87d769bce2cdc0037933a39b3cc0
Vorlent <vorlent@web.de>
2019-12-05 00:41:26 +0100

    Remove old files in repository root

--- pulseaudio.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-pactl load-module module-null-sink sink_name=cozycast-sink
-
-pactl list sink-inputs
-
-# 77 is the sink-input id of the chromium process
-pactl move-sink-input 77 cozycast-sink
