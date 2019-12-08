7f461d8f2a043908686e439315a858df4d031a37
Vorlent <vorlent@web.de>
2019-12-08 14:38:30 +0100

    Fix remote indicator

--- cozycast-server/src/main/resources/static/js/Userlist.js
+++ cozycast-server/src/main/resources/static/js/Userlist.js
@@ -4,9 +4,10 @@ export function Userlist({ state }) {
     return html`<div id="userlist" class="userlist">
         ${state.userlist.map(user => html`
             <div class="user">
-                <div class="image avatar" style="background-image: url('${user.url}');"/>
+                <div class="image avatar" style="background-image: url('${user.url}');">
+                    <i class="icon-keyboard remote" style=${user.remote ? "" : "display: none;"}></i>
+                </div>
                 <div>${user.username}</div>
-                <i class="icon-keyboard remote" style=${user.remote ? "" : "display: none;"}></i>
             </div>
         `)}
     </div>`
