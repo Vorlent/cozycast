bd3e51de2fcb158c1d6755dd03d0fcb91c521866
Vorlent <vorlent@web.de>
2019-12-25 16:53:21 +0100

    Implement second row for userlist on mobile

--- cozycast-server/src/main/resources/static/js/Userlist.js
+++ cozycast-server/src/main/resources/static/js/Userlist.js
@@ -5,6 +5,8 @@ export function Userlist({ state }) {
         ${state.userlist.map(user => html`
             <div class="user">
                 <div class="image avatar" style="background-image: url('${user.url}');">
+                </div>
+                <div class="remote-wrapper">
                     <i class="icon-keyboard remote" style=${user.remote ? "" : "display: none;"}></i>
                 </div>
                 <div>${user.username}</div>
