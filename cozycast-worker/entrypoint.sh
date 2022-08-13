3b01e7f81b0d15f23c8c393f144cc1a3dff2bf0c
Vorlent <vorlent@web.de>
2022-08-13 21:16:07 +0200

    ACTIVATE_SUDO to disable sudo by default

--- cozycast-worker/entrypoint.sh
+++ cozycast-worker/entrypoint.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+if test -z "$ACTIVATE_SUDO"; then
+    echo "no sudo"
+else
+    echo "${UNAME} ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/${UNAME}
+    chmod 0440 /etc/sudoers.d/${UNAME}
+    chown ${UID}:${GID} -R /home/${UNAME}
+    gpasswd -a ${UNAME} audio
+fi
+
 export DISPLAY=":$RANDOM"
 sudo chown cozycast:cozycast /home/cozycast
 
